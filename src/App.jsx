a37eef78338ea7871043cfa757b7cbb1ad0661a2
jhadevansh0809 <jhadevansh0809@gmail.com>
2023-03-16 10:59:57 +0530

    adding all files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Categories from "./components/Categories";
+
+const App = () => {
+  return <Product/>;
+};
+
+export default App;
